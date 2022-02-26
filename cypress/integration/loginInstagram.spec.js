247233604abfefa40ccad11dd621c51b3873bf00
Moses Ayua <lekeayua@gmail.com>
2022-02-26 12:02:14 +0100

    Second Commit Test

---
+++ cypress/integration/loginInstagram.spec.js
@@ -0,0 +1,12 @@
+/// <reference types= "cypress" />
+
+
+//This function is to describe the test suite
+describe("Instagram Login Test suite", function () {
+    
+    //This function is to describe the test case
+    it(`Instagram login`, function () {
+    //Visit instagram.
+    cy.visit("https://www.instagram.com/");
+   
+  });
